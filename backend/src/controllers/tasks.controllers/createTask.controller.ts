79906c280628c9b9fda92cb178716dc98aa359e8
nathansungu <nathanamudavi@gmail.com>
2025-07-18 14:25:47 +0300

    add create  controller

---
+++ backend/src/controllers/tasks.controllers/createTask.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import asyncHandler from "../../../utilities/ayncHandler";
+import { createTaskSchema } from "../../../zodSchemasValidation/blogs.schem";
+import { PrismaClient } from "@prisma/client";
+
+const createTasks = asyncHandler(async (req: Request, res: Response) => {
+  const client = new PrismaClient();
+  const userId = req.user!.id;
+  const userData = await createTaskSchema.parseAsync(req.body);
+  const task = { ...userData, userId };
+
+  const newBlog = await client.tasks.create({
+    data: task,
+  });
+
+  if (newBlog) {
+    res.status(201).json({ message: "Task added successfully" });
+    return;
+  }
+});
+
+export default createTasks;
