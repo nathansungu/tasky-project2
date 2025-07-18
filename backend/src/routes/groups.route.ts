f24ce7062780f76b9566cf56fd9412a9a32bcd5d
nathansungu <nathanamudavi@gmail.com>
2025-07-18 15:17:15 +0300

    add rout  grouptasks

--- backend/src/routes/groups.route.ts
+++ backend/src/routes/groups.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import createGroup from "../controllers/groups/creategroup.controller";
 import getGroups from "../controllers/groups/getUserGroups.controller";
+import groupTasks from "../controllers/groups/getTasks.controller";
 const group = Router()
 
 group.post("/",createGroup)
 group.get("/", getGroups)
+group.get("/tasks/:groupId",groupTasks)
 
 export default group;
