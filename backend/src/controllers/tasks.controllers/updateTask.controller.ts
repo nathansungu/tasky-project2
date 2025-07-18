5bd5d708435306ee4cffc7a01c4831cf4f5e1fae
nathansungu <nathanamudavi@gmail.com>
2025-07-18 14:28:32 +0300

    add update  tasks  controller

---
+++ backend/src/controllers/tasks.controllers/updateTask.controller.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "../../../utilities/ayncHandler";
+import { Request, Response } from "express";
+import { updateTaskSchema } from "../../../zodSchemasValidation/blogs.schem";
+const client = new PrismaClient();
+
+const updateTask = asyncHandler( async(req:Request, res:Response)=>{
+    const {id} = req.params;
+    const userInput = await updateTaskSchema.parseAsync(req.body)
+    const updatedTask = await client.tasks.update({
+        where:{id:id},
+        data:userInput&&userInput
+    })
+
+    return res.status(200).json({message: "Product updated successfuly"})
+})
+
+export default updateTask;
