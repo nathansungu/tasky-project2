f4fb6c5907031d9c0f0082fc733993a9c1b225d4
nathansungu <nathanamudavi@gmail.com>
2025-07-18 14:31:54 +0300

    add create group  controller

---
+++ backend/src/controllers/groups/creategroup.controller.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { Request,Response } from "express";
+import asyncHandler from "../../../utilities/ayncHandler";
+import createGroupSchema from "../../../zodSchemasValidation/group.schema";
+
+const client = new PrismaClient();
+
+const createGroup= asyncHandler(async(req: Request, res:Response )=>{
+    const userId = req.user!.id;
+    const groupdetails = await createGroupSchema.parseAsync(req.body)
+    const newGgroup =  await client.group.create({
+        data:groupdetails
+    })
+    if(newGgroup){
+        await client.groupMembers.create({
+        data:{userId:userId, role:"admin", groupId:newGgroup.id}
+    })
+
+    }
+    
+
+    return res.status(201).json({message:"Group created Succesfully", data: newGgroup})
+})
+
+export default createGroup;
