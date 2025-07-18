a17bb8c15e0eedd2b3c9ccf6ca740b8abb730d1f
nathansungu <nathanamudavi@gmail.com>
2025-07-18 14:25:15 +0300

    add complete task  controller

---
+++ backend/src/controllers/tasks.controllers/completeTask.controller.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "../../../utilities/ayncHandler";
+import { Request, Response } from "express";
+
+const client = new PrismaClient()
+
+const markComplete = asyncHandler(async(req:Request, res:Response)=>{
+    const {id}= req.params;
+
+    const completeTask = await client.tasks.update({
+        where:{id:id},
+        data:{iscompleted:true}
+    })
+
+    res.status(200).json({message:"Task Completed"})
+    return    
+})
+
+export default markComplete;
