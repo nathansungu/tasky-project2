c49a39f6b76fffa91456800b62807857fea2573a
nathansungu <nathanamudavi@gmail.com>
2025-07-22 23:26:42 +0300

    add create task page

---
+++ frontend/src/pages/createTaskPage.tsx
@@ -0,0 +1,12 @@
+import HandleCreateTask from "../components/task.components/createTask.component"
+import HandleHeader from "../components/landingPage.components/header";
+const CreateTaskPage = ()=>{
+    return(
+        <>
+        <HandleHeader/>
+        <HandleCreateTask/>
+        </>
+    )
+}
+
+export default CreateTaskPage;
