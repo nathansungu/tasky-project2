cc1ebbf560e828db56e7622ea7b60e3d0b919aa5
nathansungu <nathanamudavi@gmail.com>
2025-07-22 23:27:55 +0300

    add task and group routes

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -3,6 +3,10 @@ import LandingPage from "../pages/landingPage"
 import HandleLogin from "../pages/loginPage"
 import Register from "../pages/registrationPage"
 import DashboardPage from "../pages/dashBoard"
+import HandleTasks from "../pages/tasks"
+import CreateTaskPage from "../pages/createTaskPage"
+import GroupPage from "../pages/groupPage"
+import GroupsPage from "../pages/groupsPage"
 const HandleRouting= ()=>{
     return(
         <Routes>
@@ -10,7 +14,10 @@ const HandleRouting= ()=>{
         <Route path="/login" element={<HandleLogin/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/dashboard" element={<DashboardPage/>}/>
-           
+        <Route path="/tasks" element= {<HandleTasks/>}/>
+        <Route path="/create-task/:id?" element={<CreateTaskPage/>}/>
+        <Route path="/groups" element={<GroupsPage/>}/>
+        <Route path="/group/:id" element={<GroupPage/>}/>
 
     </Routes>
     )
