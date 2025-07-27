abb0bff161c999d743f994943cc6e056b1054ff9
nathansungu <nathanamudavi@gmail.com>
2025-07-27 18:31:44 +0300

    update main.ts file and routes file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import AuthChecker from "./utility/checkAuthentity";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <AuthChecker/>
       <App />
     </QueryClientProvider>
   </React.StrictMode>
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,27 +1,40 @@
-import {Routes, Route} from "react-router-dom"
-import LandingPage from "../pages/landingPage"
-import HandleLogin from "../pages/loginPage"
-import Register from "../pages/registrationPage"
-import DashboardPage from "../pages/dashBoard"
-import HandleTasks from "../pages/tasks"
-import CreateTaskPage from "../pages/createTaskPage"
-import GroupPage from "../pages/groupPage"
-import GroupsPage from "../pages/groupsPage"
-const HandleRouting= ()=>{
-    return(
-        <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/login" element={<HandleLogin/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<DashboardPage/>}/>
-        <Route path="/tasks" element= {<HandleTasks/>}/>
-        <Route path="/create-task/:id?" element={<CreateTaskPage/>}/>
-        <Route path="/groups" element={<GroupsPage/>}/>
-        <Route path="/group/:id" element={<GroupPage/>}/>
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/landingPage";
+import HandleLogin from "../pages/loginPage";
+import Register from "../pages/registrationPage";
+import DashboardPage from "../pages/dashBoard";
+import HandleTasks from "../pages/tasks";
+import CreateTaskPage from "../pages/createTaskPage";
+import GroupPage from "../pages/groupPage";
+import GroupsPage from "../pages/groupsPage";
+import ProfilePage from "../pages/profile";
+import TrashPage from "../pages/trash.Page";
+import Protected from "../middleware/protectedRoute";
+const HandleRouting = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<HandleLogin />} />
+      <Route path="/register" element={<Register />} />
 
+      <Route
+        path="/dashboard"
+        element={
+          <Protected>
+            <DashboardPage />
+          </Protected>
+        }
+      > 
+        
+        <Route path="" element={<HandleTasks />} />
+        <Route path="create-task/:id?" element={<CreateTaskPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="trash" element={<TrashPage />} />
+        <Route path="groups" element={<GroupsPage />} />
+        <Route path="group/:id" element={<GroupPage />} />
+      </Route>
     </Routes>
-    )
-    
-}
+  );
+};
 
-export default HandleRouting
+export default HandleRouting;
