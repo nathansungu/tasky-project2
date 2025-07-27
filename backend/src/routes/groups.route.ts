845cfbc065a8146b2cefa8bb0cc39b46d6d10871
nathansungu <nathanamudavi@gmail.com>
2025-07-27 18:16:46 +0300

    addroutes to the added groups and uder controllers

--- backend/src/routes/groups.route.ts
+++ backend/src/routes/groups.route.ts
@@ -3,11 +3,17 @@ import createGroup from "../controllers/groups/creategroup.controller";
 import getGroups from "../controllers/groups/getUserGroups.controller";
 import groupTasks from "../controllers/groups/getTasks.controller";
 import addMember from "../controllers/groups/addMember.controller";
-const group = Router()
+import getGroup from "../controllers/groups/getGroup.controller";
+import getMembers from "../controllers/groups/getGroupMembers.controller";
+import removeUser from "../controllers/groups/removeUser.controller";
+const group = Router();
 
-group.post("/",createGroup)
-group.get("/", getGroups)
-group.get("/tasks/:groupId",groupTasks)
-group.post("/members",addMember)
+group.post("/", createGroup);
+group.get("/", getGroups);
+group.get("/members/:id", getMembers);
+group.get("/:id", getGroup);
+group.get("/tasks/:groupId", groupTasks);
+group.patch("/members", addMember);
+group.patch("/member/:id", removeUser)
 
 export default group;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import updateProfile from "../controllers/user.controllers/updateProfille.controller";
 import changePassword from "../controllers/user.controllers/changePassword.controller";
 import logedinUser from "../controllers/authentication/logedInuser.controller";
-import users from "../controllers/user.controllers/getUserDetails.controller";
+import users from "../controllers/user.controllers/getAllUsersDetails.controller";
+import userProfile from "../controllers/user.controllers/getUserProfile.controller";
 const user = Router();
 
 user.patch("/", updateProfile);
 user.patch("/password", changePassword);
 user.get("/",users)
-user.get("/me",logedinUser)
+user.get("/:id",userProfile)
+user.post("/me",logedinUser)
 
 export default user;
