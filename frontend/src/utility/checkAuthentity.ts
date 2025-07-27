398b99ac1966289ccc0738f598b48aa2a451b079
nathansungu <nathanamudavi@gmail.com>
2025-07-27 21:54:10 +0300

    drop /me route

--- frontend/src/utility/checkAuthentity.ts
+++ frontend/src/utility/checkAuthentity.ts
@@ -1,25 +1,25 @@
-import { useEffect } from 'react'
-import useUserStore from '../store/logedinState.store'
-import axiosInstance from '../api/axios'
+// import { useEffect } from 'react'
+// import useUserStore from '../store/logedinState.store'
+// // import axiosInstance from '../api/axios'
 
-const AuthChecker = () => {
-  const { setUser } = useUserStore()
+// const AuthChecker = () => {
+//   const { setUser } = useUserStore()
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      try {
-        const response = await axiosInstance.post("/user/me")
-        const { data } = response.data  
-        setUser(data) 
-      } catch (e) {
-        setUser(null) 
-      }
-    }
+//   // useEffect(() => {
+//   //   const checkLogin = async () => {
+//   //     try {
+//   //       // const response = await axiosInstance.post("/user/me")
+//   //       // const { data } = response.data  
+//   //       setUser(data) 
+//   //     } catch (e) {
+//   //       setUser(null) 
+//   //     }
+//   //   }
 
-    checkLogin()
-  }, [setUser])
+//     checkLogin()
+//   }, [setUser])
 
-  return null
-}
+//   return null
+// }
 
-export default AuthChecker
+// export default AuthChecker
