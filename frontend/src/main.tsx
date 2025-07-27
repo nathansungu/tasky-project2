d72baba91e0cd12acd73c4eb93b96ad84b5a6e7f
nathansungu <nathanamudavi@gmail.com>
2025-07-27 22:29:10 +0300

    update /me route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import AuthChecker from "./utility/checkAuthentity";
+import AuthChecker from "./utility/checkAuthentity";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* <AuthChecker/> */}
+      <AuthChecker/>
       <App />
     </QueryClientProvider>
   </React.StrictMode>
