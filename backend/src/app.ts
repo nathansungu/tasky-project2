5d0bcf00246492e84b22221726e2c691a00e6494
nathansungu <nathanamudavi@gmail.com>
2025-07-27 21:06:58 +0300

    add cors origin to match vercel host

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://tasky-project2.vercel.app",
     credentials: true,
   })
 );
