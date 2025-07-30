8762a819617cd60181b0ac1734930f23d113c404
nathansungu <nathanamudavi@gmail.com>
2025-07-31 02:14:31 +0300

    add imgurl to group  schema

---
+++ backend/prisma/migrations/20250730231250_add_group_url/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."group" ADD COLUMN     "imgUrl" TEXT;
--- backend/zodSchemasValidation/group.schema.ts
+++ backend/zodSchemasValidation/group.schema.ts
@@ -2,7 +2,8 @@ import z from "zod";
 
 export const createGroupSchema = z.object({
     name: z.string(),
-    description: z.string()
+    description: z.string(),
+    imgUrl: z.string().optional()
 })
 export const addGroupMemberSchema = z.object({
     userId: z.string(),
