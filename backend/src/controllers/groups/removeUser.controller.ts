2c81c1a93e413c42dee5e8d1fc33f2aec2ddca13
nathansungu <nathanamudavi@gmail.com>
2025-07-30 12:52:37 +0300

    update remove user from group logic

--- backend/src/controllers/groups/removeUser.controller.ts
+++ backend/src/controllers/groups/removeUser.controller.ts
@@ -1,21 +1,20 @@
-import { PrismaClient } from '@prisma/client';
-import asyncHandler from '../../../utilities/ayncHandler';
-import { Request, Response } from 'express';
-const client = new PrismaClient()
-const removeUser = asyncHandler(async(req:Request, res:Response)=>{
-    const userId = req.user!.id
-    const {id }= req.params
-    const user = await client.groupMembers.update({
-        where:{id:id, userId:userId,},
-        data:{isDeleted:true}
-    })
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "../../../utilities/ayncHandler";
+import { Request, Response } from "express";
+const client = new PrismaClient();
+const removeUser = asyncHandler(async (req: Request, res: Response) => {
+  const data = req.params;
+  const user = await client.groupMembers.update({
+    where: { id: data.groupId, userId: data.userId },
+    data: { isDeleted: true },
+  });
 
-    if (user) {
-        res.status(200).json({message:"User deleted succesfuly"})
-        return
-    }
+  if (user) {
+    res.status(200).json({ message: "User deleted succesfuly" });
+    return;
+  }
 
-    return res.status(400).json({Message:" Failed to delete user"})
-})
+  return res.status(400).json({ Message: " Failed to delete user" });
+});
 
 export default removeUser;
